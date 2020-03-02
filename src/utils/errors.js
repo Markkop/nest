38005cf6de8cf1c65565c3d20676f213ab45ba26
Markkop <marcelo.kopmann@linx3.com>
2020-03-02 12:46:28 -0300

    refac: remove unused code

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,20 +1,14 @@
-const { Errors } = require('moleculer')
+const { Errors } = require("moleculer");
 
 class AsanaError extends Errors.MoleculerError {
-  constructor (msg, error) {
-    super(`${msg}: ${error.message}`, error.status, 'ASANA_ERROR', error.value)
-  }
+	constructor(msg, error) {
+		super(
+			`${msg}: ${error.message}`,
+			error.status,
+			"ASANA_ERROR",
+			error.value
+		);
+	}
 }
 
-class MongooseError extends Errors.MoleculerError {
-  constructor (msg, error) {
-    super(
-      `${msg}: ${error.message}`,
-      error.status,
-      'MONGOOSE_ERROR',
-      error.value
-    )
-  }
-}
-
-module.exports = { AsanaError, MongooseError }
+module.exports = { AsanaError };
